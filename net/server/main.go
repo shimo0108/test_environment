24fce505a34c7d89368412470ad234f936fe7499
下笹　翔太 <syk8153181531@gmail.com>
2022-09-26 23:16:35 +0900

    add server

---
+++ net/server/main.go
@@ -0,0 +1,85 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+)
+
+func isGZipAcceptable(request *http.Request) bool {
+	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","), "gzip") != -1
+}
+
+func processSession(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+	for {
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			}
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		response := http.Response{
+			StatusCode: 200,
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header:     make(http.Header),
+		}
+		if isGZipAcceptable(request) {
+
+			content := "Hello World (gzipped)\n"
+			var buffer bytes.Buffer
+			writer := gzip.NewWriter(&buffer)
+			io.WriteString(writer, content)
+			writer.Close()
+			response.Body = io.NopCloser(&buffer)
+			response.ContentLength = int64(buffer.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		}
+		if !isGZipAcceptable(request) {
+			content := "Hello World\n"
+			response.Body = io.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+		response.Write(conn)
+	}
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("cannot listen", err)
+	}
+	fmt.Println("Server is running at localhost:8080")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("cannot accept", err)
+			panic(err)
+		}
+		go processSession(conn)
+	}
+}
